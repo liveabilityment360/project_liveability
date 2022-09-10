96b4db75267df2f82164b2b1ece096122088e023
liveabilityment360 <109857290+liveabilityment360@users.noreply.github.com>
2022-09-10 20:23:40 +1000

    Update childcarecentre.sql

--- dbt/batch-dbt-bq-main/models/childcarecentre.sql
+++ dbt/batch-dbt-bq-main/models/childcarecentre.sql
@@ -14,7 +14,7 @@ with transform_child_care_centre as (
         ,SAFE_CAST(Latitude as FLOAT64) as Latitude
         ,SAFE_CAST(Longitude as FLOAT64) as Longitude
         ,ST_GeogPoint(SAFE_CAST(Longitude as FLOAT64),SAFE_CAST(Latitude as FLOAT64)) as Location
-    from liveability-demo.batchdata.childcarecenters
+    from ment360-liveability-alpha.liveability.childcarecenters
 )
 
 select * from transform_child_care_centre
